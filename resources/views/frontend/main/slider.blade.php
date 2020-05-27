3a97990e8fa701444975c87938f3ece3e6468f2b
laxmi dhakal <laxmidhakal159@gmail.com>
2020-05-27 11:25:57 +0545

    publication design

--- resources/views/frontend/main/slider.blade.php
+++ resources/views/frontend/main/slider.blade.php
@@ -15,26 +15,24 @@
     <div class="col-md-4">
       <div class="thumbnail ">
         <img src="{{URL::to('/')}}/images/slider/hero-1.jpg" alt="..."  style="height: 107px; width: 100%;">
-          <div class="col-sm-6">
+          <div class="col-sm-6 viewdiv">
             <a href="#" class="btn btncssview btn-warning btn-block" role="button" id="color">View</a>
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-6 orderdiv">
             <a href="#" class="btn btncssorder btn-default btn-block" role="button" id="color">Order</a>
           </div>
       </div>
     </div>
     <div class="col-md-4">
-      <div class="thumbnail">
+      <div class="thumbnail ">
         <img src="{{URL::to('/')}}/images/slider/hero-1.jpg" alt="..."  style="height: 107px; width: 100%;">
+          <div class="col-sm-6 viewdiv">
+            <a href="#" class="btn btncssview btn-warning btn-block" role="button" id="color">View</a>
+          </div>
+          <div class="col-sm-6 orderdiv">
+            <a href="#" class="btn btncssorder btn-default btn-block" role="button" id="color">Order</a>
+          </div>
       </div>
-      <div class="col-sm-6">
-        <a href="#" class="btn btn-warning btn-block" role="button" id="color">View</a>
-      </div>
-      <div class="col-sm-6">
-        <a href="#" class="btn btn-warning btn-block" role="button" id="color">View</a>
-      </div>
-      
-      
     </div>
     <div class="col-md-4">
       <h2 class="section-title"> Publicity/Advertising site</h2>
